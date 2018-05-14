56a5151b6aae9076486eef5695bda5a803332e18
Erick Canto <kucho_super.68@hotmail.com>
2018-05-14 16:00:51 +0000

    Se agregó ejemplos de Pilas y templates

--- Pila.cpp
+++ Pila.cpp
@@ -16,3 +16,44 @@ class Pila
         Tipo Pop();
     
 };
+
+template<typename Tipo>
+Pila<Tipo>::Pila(int tam)
+{
+    tope=0;
+    max=tam;
+    
+    arreglo = new Tipo[max];
+}
+
+template<typename Tipo>
+Pila<Tipo>::~Pila
+{
+    delete [ ] arreglo;
+}
+
+template<typename Tipo>
+void Pila<Tipo>::Push(Tipo dato)
+{
+    
+}
+
+template<typename Tipo>
+Tipo Pila<Tipo>::Pop()
+{
+    
+}
+
+
+int main()
+{
+    Pila<int> miPila{5};
+    
+    miPila.Push(3);
+    miPila.Push(7);
+    
+    int x=miPila.Pop();
+    std::cout<<x<<std::endl;
+    int x=miPila.Pop();
+    std::cout<<x<<std::endl;
+}
